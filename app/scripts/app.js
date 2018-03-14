305c3b56624e664c3948778f3cdbbfcbaf6e1ba5
Igor Gorlov <gorlov35@gmail.com>
2018-03-14 18:23:33 +0300

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+import svg4everybody from 'svg4everybody';
+// import $ from 'jquery';
+// import slick from 'slick-carousel';
+// import 'magnific-popup';
+// import mask from "jquery-mask-plugin";
+// import 'jquery-validation';
+// import printThis from "print-this";
+
+(function ($) {
+
+	var styles = [
+		'padding: 0 9px',
+		'background: #fff',
+		'color: #000',
+		'display: inline-block',
+		'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.2)',
+		'box-shadow: 0 -1px 0 rgba(255, 255, 255, 0.2) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset',
+		'line-height: 1.4',
+		'text-align: left',
+		'font-size: 12px',
+		'font-weight: 400'
+	].join(';');
+
+	console.log('%c заказать html верстку', styles);
+	console.log('%c gorlov35@gmail.com', styles);
+
+	$(function() {
+		svg4everybody();
+
+
+	});
+
+})(jQuery);
